2ad309f8e02c170acba9b52e2479c518241471ea
Inna Kozhina <student@BC-minimac006s-Mac-mini.local>
2022-08-16 15:00:35 +0300

    Solution for Military task

---
+++ 2 week/military task/main.js
@@ -0,0 +1,13 @@
+let age = prompt("How old are you?");
+
+if (age < 18) {
+    console.log('too young');
+} else if (age < 27) {
+    console.log('Right age for military service')
+} else if (age < 41) {
+    console.log('You are in reserve')
+} else if (age < 55) {
+    console.log('You are in backup reserve')
+} else {
+    console.log('too aged')
+}
---
+++ 2 week/task_2.js
@@ -0,0 +1,37 @@
+// Guess what answers you would get if you ran this in the Javascript Console.
+//Once you have an answer to the questions then check them by copying them and running it in the console yourself line by line 
+
+
+//Evaluate the below:
+something! //
+3 + "65" // NaN - "365"
+7 - "3" // NaN - 4
+15 % 5 // 3 - 0
+5 % 15 // 0 - 5
+"Java" + "Script" // 'JavaScript'
+"Java" - "Script" // Error - NaN
+" " + " " // 2 spaces in '  '
+" " + 0 // NaN - ' 0'
+true + true // true - 2
+true + false // false - 1
+false + true // false - 1
+false - true // false - -1
+3 - 4 // -1
+
+
+//Evaluate the below comparisons:
+4 >= 2 // false - true
+6 === 2 // false
+3 <= 1 // false
+1 != 1 // false
+"A" > "B" // true - false
+"B" < "C" // true
+"a" > "A" // true
+"b" < "A" // true
+true === false // true
+true != true // true - false
+
+
+// Make the string: 
+// "Go To Helsinki Business College," they said. "It'll Will Be "Fun"," they said.
+// by using the + sign: "\ + 'Go' + 'To' Helsinki Business College," they said. "It'll Will Be "Fun"," they said.
