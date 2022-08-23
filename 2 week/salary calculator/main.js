d402579dcd109f2691c196c843b80d7ff2529dd1
Inna Kozhina <student@BC-minimac006s-Mac-mini.local>
2022-08-23 09:40:40 +0300

    salary calculator

--- 2 week/salary calculator/main.js
+++ 2 week/salary calculator/main.js
@@ -1,21 +1,20 @@
 const daySalary = () => {
     const payPerHour = Number(prompt('How much do you get per hour?'));
     const hoursPerDay = Number(prompt('How many hours have you worked today?'));
-
-    let basicSalary = 7 * payPerHour;
-    const halfExtra = payPerHour + (payPerHour / 2);
-    // const doubleExta = basicSalary + ((hoursPerDay - 7) * * 2;
     let salary;
+    const basicSalary = 7 * payPerHour;
+    const halfExtra = payPerHour * 1,5;
+    const doubleExta = payPerHour * 2;
+    
     if (hoursPerDay <= 7) {
-        salary = payPerHour * hoursPerDay;
-        return console.log(salary);
+    salary = payPerHour * hoursPerDay;
+    return console.log(salary);
     } else if (hoursPerDay > 7 && hoursPerDay < 10) {
-        salary = basicSalary + ((hoursPerDay - 7) * halfExtra);
-        return console.log(salary);
+    salary = basicSalary + ((hoursPerDay - 7) * halfExtra);
+    return console.log(salary);
     } else {
-        salary = (7 * payPerHour)  + ((hoursPerDay - 9) * doubleExta);
-        return console.log(salary);
+    salary = basicSalary + (2 * halfExtra) + ((hoursPerDay - 9) * doubleExta);
+    return console.log(salary);
     }
-}
-
-daySalary();
+    }
+    daySalary();
